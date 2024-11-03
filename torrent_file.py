c120ebf7d749a894da61b1acc15236a7c1c746e8
nghiaho310pf <nghia.ho310pf@hcmut.edu.vn>
2024-11-03 12:47:57 +0700

    Simplify file piecing a bit

--- torrent_file.py
+++ torrent_file.py
@@ -1,8 +1,12 @@
 class TorrentFile:
+    path: str
+    byte_count: int
+    byte_start: int
+
     def __init__(self, path, byte_count):
         self.path = path
         self.byte_count = byte_count
-        self.byte_start = None
+        self.byte_start = 0
 
     def set_byte_start(self, start):
         self.byte_start = start
@@ -11,11 +15,16 @@ class TorrentFile:
         return f"File(path={self.path}, byte_count={self.byte_count}, byte_start={self.byte_start})"
 
 class PieceSection:
-    def __init__(self, file_index, file_offset, piece_offset, length):
+    length: int
+    file_index: int
+    file_offset: int
+    piece_offset: int
+
+    def __init__(self, length, file_index, file_offset, piece_offset):
+        self.length = length
         self.file_index = file_index
         self.file_offset = file_offset
         self.piece_offset = piece_offset
-        self.length = length
 
     def __repr__(self):
         return f"PieceSection(file_index={self.file_index}, file_offset={self.file_offset}, piece_offset={self.piece_offset}, length={self.length})"
@@ -32,29 +41,23 @@ class Piece:
 
 def pack_files_to_pieces(files: list[TorrentFile], piece_size):
     pieces = []
-    print(f" -> Piece 0")
     current_piece = Piece()
     current_piece_position = 0
 
     current_start = 0
     for file_index, file in enumerate(files):
         file.set_byte_start(current_start)
-        print(f"Processing file {file.path} ({file.byte_count}, starting at byte {current_start})")
 
         current_file_position = 0
         while current_file_position < file.byte_count:
-            print(f"... {current_piece_position} | {file.byte_count - current_file_position} >= {piece_size - current_piece_position}")
             if (file.byte_count - current_file_position) >= (piece_size - current_piece_position): # need a new piece
-                print(f"          -> [A] Adding section: file[{current_file_position} -> {current_file_position + piece_size - current_piece_position}] piece[{current_piece_position} -> {current_piece_position + piece_size - current_piece_position}]")
-                current_piece.add_section(PieceSection(file_index, current_file_position, current_piece_position, piece_size - current_piece_position))
+                current_piece.add_section(PieceSection(piece_size - current_piece_position, file_index, current_file_position, current_piece_position))
                 current_file_position += piece_size - current_piece_position
                 pieces.append(current_piece)
-                print(f" -> Piece {len(pieces)}")
                 current_piece = Piece()
                 current_piece_position = 0
             else:
-                print(f"          -> [B] Adding section: file[{current_file_position} -> {current_file_position + file.byte_count - current_file_position}] piece[{current_piece_position} -> {current_piece_position + file.byte_count - current_file_position}]")
-                current_piece.add_section(PieceSection(file_index, current_file_position, current_piece_position, (file.byte_count - current_file_position)))
+                current_piece.add_section(PieceSection(file.byte_count - current_file_position, file_index, current_file_position, current_piece_position))
                 current_piece_position += file.byte_count - current_file_position
                 current_file_position = file.byte_count
 
